82ed365b626cb965c6169b6370a1fa4a4a60a483
ram33 <ramesh0533@gmail.com>
2024-09-01 15:33:38 +0530

    Sample .env and error.jsx

---
+++ observecart-frontend/pages/_error.js
@@ -0,0 +1,23 @@
+function Error({ statusCode }) {
+  return (
+    <p>
+      {statusCode
+        ? `An error ${statusCode} occurred on server`
+        : "An error occurred on client"}
+    </p>
+  );
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  if (typeof window == "undefined") {
+    const newrelic = require("newrelic");
+    newrelic.noticeError(err);
+  } else {
+    window.newrelic.noticeError(err);
+  }
+
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
